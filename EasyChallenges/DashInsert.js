e01adcf1753fb89ce2627de8e7872a1ee2ffd122
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-11-20 00:00:30 -0600

    Add DashInsert Challenge

---
+++ EasyChallenges/DashInsert.js
@@ -0,0 +1,17 @@
+function DashInsert(str) {
+  var numbers = str.toString().split(""), newArray = [];
+  for(var i = 0; i<numbers.length;i++){
+    if(numbers[i] % 2 !== 0 && numbers[i+1] % 2 === 1){
+      newArray.push(numbers[i]);
+      newArray.push("-");
+    } else {
+      newArray.push(numbers[i]);
+    }
+  }
+  return newArray.join("");
+
+}
+
+// keep this function call here
+// to see how to enter arguments in JavaScript scroll down
+DashInsert(readline());
